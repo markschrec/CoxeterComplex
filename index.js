bde3292ab22f9fc474af1822627bcb9f65079a57
markschrec <mas3er@virginia.edu>
2021-05-17 22:42:49 -0400

    made css and js file, initial form elements

---
+++ index.js
@@ -0,0 +1,10 @@
+function generateCoxeterComplex(){
+    document.getElementById("output").innerHTML = "You Fool! I just started this tonight.<br> This functionality is not implemented yet.";
+}
+
+function clearPage(){
+    document.getElementById("output").innerHTML = "The Coxeter Complex will be displayed here.";
+    document.getElementById("mst").value = "";
+    document.getElementById("msu").value = "";
+    document.getElementById("mtu").value = "";
+}
