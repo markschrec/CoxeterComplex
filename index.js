a3b1a03b4d00c3bea01009ccc6920e4ec2a5c942
markschrec <mas3er@virginia.edu>
2021-05-18 01:52:09 -0400

    form validation, need to refactor index.html code

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ function generateCoxeterComplex(){
 
 function clearPage(){
     document.getElementById("output").innerHTML = "The Coxeter Complex will be displayed here.";
-    document.getElementById("mst").value = "";
-    document.getElementById("msu").value = "";
-    document.getElementById("mtu").value = "";
+    document.getElementById("mst").value = "2";
+    document.getElementById("mst").disabled = false;
+    document.getElementById("msu").value = "2";
+    document.getElementById("msu").disabled = false;
+    document.getElementById("mtu").value = "2";
+    document.getElementById("mtu").disabled = false;
+    document.getElementById("mstinf").checked = false;
+    document.getElementById("msuinf").checked = false;
+    document.getElementById("mtuinf").checked = false;
+}
+
+function toggleInput(checkbox, id){
+    document.getElementById(id).disabled = checkbox.checked;
 }
